759e8430ab7b897b82cf90b5b818ced78fa65db1
Sunrunchen <runchen.sun@gmail.com>
2017-10-02 11:11:06 -0500

    Update userShares.php

--- views/userShares.php
+++ views/userShares.php
@@ -5,6 +5,7 @@ require_once('../includes/smarty.inc.php');//smarty模板配置
 require_once('../includes/connect.php');
 require_once('../includes/userShell.php');
 session_start();
+ <input type="text"  name="fxurl" value="<https://share.any-share.space/>
 if($isVisitor == "true"){
 	echo '<script>window.location.href="login.php";</script>';
 	exit();
@@ -13,7 +14,7 @@ $result1 = mysqli_query($con,"SELECT * FROM sd_setting");//获取数据
 while($row1 = mysqli_fetch_assoc($result1)){ 
 	$tit= $row1['main_tit'];
 	$theme= $row1['theme'];
-	$url= $row1['zzurl'];
+	$url= $row1['fxurl'];
 	$domain= $row1['kjurl'];
 	$tjcode= $row1['tjcode'];
 }
@@ -47,7 +48,7 @@ $jscode=$tjcode;
 $smarty->assign("tit", $tit);
 $smarty->assign("filenum", $userFile);  
 $smarty->assign("sharenum", $userShare);  
-$smarty->assign("zzurl", $url); 
+$smarty->assign("fxurl", $url); 
 $smarty->assign("gname", $groupName); 
 $smarty->assign("isVisitor", $isVisitor); 
 $smarty->assign("userinfo", $userInfo); 
